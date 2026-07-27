9f9fd08d4f9133bf8416e457c4a9347aca70ca8e
fogelmark <108455196+fogelmark@users.noreply.github.com>
2026-07-27 21:20:28 +0200

    added font

---
+++ lib/fonts.ts
@@ -0,0 +1,11 @@
+import localFont from "next/font/local"
+
+export const leaguegothic = localFont({
+	src: [
+		{
+			path: "../public/fonts/LeagueGothic-Regular.woff2",
+			weight: "400",
+			style: "normal",
+		},
+	],
+})
